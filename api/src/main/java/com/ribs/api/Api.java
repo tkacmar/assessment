12bafcddda13d452ccd1c4f5b94968119e7565cd
Tomas Kacmar <ing.kacmar.tomas@gmail.com>
2021-08-18 17:38:09 +0200

    Project base structure with 2 modules

---
+++ api/src/main/java/com/ribs/api/Api.java
@@ -0,0 +1,9 @@
+package com.ribs.api;
+
+public class Api {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from API service");
+    }
+
+}
---
+++ logger/src/main/java/com/ribs/logger/Logger.java
@@ -0,0 +1,9 @@
+package com.ribs.logger;
+
+public class Logger {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from LOGGER service");
+    }
+
+}
